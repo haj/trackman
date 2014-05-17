8df18ab31c753802aeb05f47829ed247031404f9
Zakaria Braksa <zakaria.braksa@gmail.com>
2014-05-18 00:10:36 +0100

    implemented logic to get the last few positions for a given device

--- app/models/traccar/device.rb
+++ app/models/traccar/device.rb
@@ -14,4 +14,10 @@ class Traccar::Device < ActiveRecord::Base
   			return {longitude: position.longitude, latitude: position.latitude }
   		end
   	end
+
+    def last_positions(number)
+      return self.positions.order("time DESC").limit(number)
+    end
+
+
 end
