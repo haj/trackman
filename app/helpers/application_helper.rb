9f4a731ff715a06c53a276bfff9225fd00e2c43f
Zakaria Braksa <zakaria.braksa@gmail.com>
2014-07-07 10:31:04 +0000

    integrated notifications view with the template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
 	def notifications_count(user)
 		user.mailbox.notifications.count
 	end
+
+	def notification_title(notification)
+		notification.subject.split(":")[0].strip
+	end
+
+	def notification_desc(notification)
+		notification.subject.split(":")[1].strip
+	end
+
 end
