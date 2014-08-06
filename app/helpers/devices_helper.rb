07ff298ee7297b495dc133929094958b148d9e78
Zakaria Braksa <zakaria.braksa@gmail.com>
2014-08-06 09:12:59 +0100

    updated all views

--- app/helpers/devices_helper.rb
+++ app/helpers/devices_helper.rb
@@ -9,15 +9,15 @@ module DevicesHelper
 	end
 
 	def list_available_simcards
-		return select_tag('simcard_id', content_tag(:option,'Select Simcard ...', :value=>"")+options_from_collection_for_select(Simcard.available_simcards, 'id', 'name'))
+		return select_tag('simcard_id', content_tag(:option,'Select Simcard ...', :value=>"")+options_from_collection_for_select(Simcard.available_simcards, 'id', 'name'), class: "form-control")
 	end
 
 	def list_no_available_simcards
-		return select_tag('simcard_id', content_tag(:option,'No available simcards', :value=>""))
+		return select_tag('simcard_id', content_tag(:option,'No available simcards', :value=>""), class: "form-control")
 	end
 
 	def list_simcards_with_default(simcard)
-		return select_tag('simcard_id', content_tag(:option, simcard.name, :value=> simcard.id )+options_from_collection_for_select(Simcard.available_simcards, 'id', 'name'))
+		return select_tag('simcard_id', content_tag(:option, simcard.name, :value=> simcard.id )+options_from_collection_for_select(Simcard.available_simcards, 'id', 'name'), class: "form-control")
 	end
 
 	 
