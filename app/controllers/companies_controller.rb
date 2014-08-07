825b24b10980532f3799f16f41cbf5c0ac7a0b10
Zakaria Braksa <zakaria.braksa@gmail.com>
2014-08-07 18:02:11 +0100

    now the user is able to change time zone after the account is created

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -75,6 +75,6 @@ class CompaniesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def company_params
-      params.require(:company).permit(:name, :subdomain)
+      params.require(:company).permit(:name, :subdomain, :time_zone)
     end
 end
