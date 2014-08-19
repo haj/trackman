6efcfdecd2f5930bb894c1c8e46d8a6d2e3c4294
Zakaria Braksa <zakaria.braksa@gmail.com>
2014-08-19 11:25:43 +0100

    reactivated reverse geocoding

--- app/models/traccar/position.rb
+++ app/models/traccar/position.rb
@@ -36,8 +36,7 @@ class Traccar::Position < ActiveRecord::Base
     end
   end
 
-  #after_validation :reverse_geocode
-
+  after_validation :reverse_geocode
 
   def car
     Device.where(emei: self.device.uniqueId).first.car
