c291ab7ef051552477b8aa957ed6220ce2661970
Zakaria Braksa <zakaria.braksa@gmail.com>
2014-09-14 16:07:50 +0100

    streamlined the UI

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -108,6 +108,6 @@ class GroupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
-      params.require(:group).permit(:name, :work_schedule_id, alarm_ids: [])
+      params.require(:group).permit(:name, alarm_ids: [])
     end
 end
