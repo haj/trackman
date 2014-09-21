ea0cc69389ee1f515d35c797ee6caedfba06c852
Zakaria Braksa <zakaria.braksa@gmail.com>
2014-09-21 03:16:46 +0100

    removed useless model

--- app/models/pricing.rb
+++
@@ -1,8 +0,0 @@
-class Pricing < ActiveRecord::Base
-	belongs_to :plan
-
-
-	def generate_paymill_record
-		Paymill::Offer.create amount: self.amount , currency: "USD", interval: self.billable_days , name: self.plan.name
-	end
-end
