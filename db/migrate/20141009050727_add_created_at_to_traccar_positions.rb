bcc2c3cb864e4c2a7cce6bab2219e9b82e94f2a6
Zakaria Braksa <zakaria.braksa@gmail.com>
2014-10-22 16:42:15 +0100

    Added a new migration that will automatically add the new column in traccar's positions table

--- db/migrate/20141009050727_add_created_at_to_traccar_positions.rb
+++ db/migrate/20141009050727_add_created_at_to_traccar_positions.rb
@@ -3,6 +3,7 @@ class AddCreatedAtToTraccarPositions < ActiveRecord::Migration
 	ActiveRecord::Base.establish_connection("secondary_#{Rails.env}".to_sym)
 
   def change
+  	ActiveRecord::Base.connection.initialize_schema_migrations_table
     execute "ALTER TABLE `positions` ADD `created_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;" 
     #@connection = ActiveRecord::Base.establish_connection("#{Rails.env}").connection
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,420 +11,66 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925103916) do
+ActiveRecord::Schema.define(version: 20141009050727) do
 
-  create_table "alarm_notifications", force: true do |t|
-    t.integer  "car_id"
-    t.integer  "driver_id"
-    t.integer  "alarm_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "company_id"
-    t.boolean  "archived",   default: false
-  end
-
-  create_table "alarms", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "description"
-    t.datetime "deleted_at"
-  end
-
-  add_index "alarms", ["deleted_at"], name: "index_alarms_on_deleted_at"
-
-  create_table "alarms_cars", force: true do |t|
-    t.integer  "car_id",     null: false
-    t.integer  "alarm_id",   null: false
-    t.string   "status"
-    t.datetime "last_alert"
-  end
-
-  add_index "alarms_cars", ["car_id", "alarm_id"], name: "index_alarms_cars_on_car_id_and_alarm_id", unique: true
-
-  create_table "alarms_groups", force: true do |t|
-    t.integer  "group_id",   null: false
-    t.integer  "alarm_id",   null: false
-    t.string   "status"
-    t.datetime "last_alert"
-  end
-
-  add_index "alarms_groups", ["group_id", "alarm_id"], name: "index_alarms_groups_on_group_id_and_alarm_id", unique: true
-
-  create_table "alarms_rules", force: true do |t|
-    t.integer  "rule_id",     null: false
-    t.integer  "alarm_id",    null: false
-    t.string   "conjunction"
-    t.string   "params"
-    t.datetime "deleted_at"
-  end
-
-  add_index "alarms_rules", ["alarm_id", "rule_id"], name: "index_alarms_rules_on_alarm_id_and_rule_id", unique: true
-  add_index "alarms_rules", ["deleted_at"], name: "index_alarms_rules_on_deleted_at"
-
-  create_table "car_manufacturers", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "car_models", force: true do |t|
-    t.string   "name"
-    t.integer  "car_manufacturer_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "car_types", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "cars", force: true do |t|
-    t.float    "mileage"
-    t.string   "numberplate"
-    t.integer  "car_model_id"
-    t.integer  "car_type_id"
-    t.string   "registration_no"
-    t.integer  "year"
-    t.string   "color"
-    t.integer  "group_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "company_id"
-    t.integer  "work_schedule_id"
-    t.string   "name"
-    t.datetime "deleted_at"
-  end
-
-  add_index "cars", ["deleted_at"], name: "index_cars_on_deleted_at"
-
-  create_table "companies", force: true do |t|
-    t.string   "name"
-    t.string   "subdomain"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "plan_id"
-    t.string   "time_zone"
-  end
-
-  create_table "delayed_jobs", force: true do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
-
-  create_table "device_manufacturers", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "device_models", force: true do |t|
-    t.string   "name"
-    t.integer  "device_manufacturer_id"
-    t.string   "protocol"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "device_types", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "application_settings", force: true do |t|
+    t.boolean "registrationEnabled", null: false
   end
 
   create_table "devices", force: true do |t|
-    t.string   "name"
-    t.string   "emei"
-    t.float    "cost_price"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "device_model_id"
-    t.integer  "device_type_id"
-    t.integer  "car_id"
-    t.integer  "company_id"
-    t.boolean  "movement"
-    t.datetime "last_checked"
-    t.datetime "deleted_at"
+    t.string  "name"
+    t.string  "uniqueId"
+    t.integer "latestPosition_id", limit: 8
   end
 
-  add_index "devices", ["deleted_at"], name: "index_devices_on_deleted_at"
+  add_index "devices", ["latestPosition_id"], name: "FK5CF8ACDD7C6208C3", using: :btree
 
-  create_table "features", force: true do |t|
-    t.string   "name"
-    t.string   "role"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "positions", force: true do |t|
+    t.string    "address"
+    t.float     "altitude",   limit: 53
+    t.float     "course",     limit: 53
+    t.float     "latitude",   limit: 53
+    t.float     "longitude",  limit: 53
+    t.string    "other"
+    t.float     "power",      limit: 53
+    t.float     "speed",      limit: 53
+    t.datetime  "time"
+    t.boolean   "valid"
+    t.integer   "device_id",  limit: 8
+    t.timestamp "created_at",            null: false
   end
 
-  create_table "features_plan_types", force: true do |t|
-    t.integer "feature_id",   null: false
-    t.integer "plan_type_id", null: false
-    t.string  "active"
-  end
+  add_index "positions", ["device_id", "time"], name: "positionsIndex", using: :btree
+  add_index "positions", ["device_id"], name: "FK65C08C6ADB0C3B8A", using: :btree
 
-  add_index "features_plan_types", ["feature_id", "plan_type_id"], name: "index_features_plan_types_on_feature_id_and_plan_type_id", unique: true
-
-  create_table "group_work_hours", force: true do |t|
-    t.integer  "day_of_week"
-    t.datetime "starts_at"
-    t.datetime "ends_at"
-    t.integer  "group_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "groups", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "company_id"
-  end
-
-  create_table "locations", force: true do |t|
-    t.string   "address"
-    t.integer  "position_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "mailboxer_conversation_opt_outs", force: true do |t|
-    t.integer "unsubscriber_id"
-    t.string  "unsubscriber_type"
-    t.integer "conversation_id"
-  end
-
-  create_table "mailboxer_conversations", force: true do |t|
-    t.string   "subject",    default: ""
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-  end
-
-  create_table "mailboxer_notifications", force: true do |t|
-    t.string   "type"
-    t.text     "body"
-    t.string   "subject",              default: ""
-    t.integer  "sender_id"
-    t.string   "sender_type"
-    t.integer  "conversation_id"
-    t.boolean  "draft",                default: false
-    t.datetime "updated_at",                           null: false
-    t.datetime "created_at",                           null: false
-    t.integer  "notified_object_id"
-    t.string   "notified_object_type"
-    t.string   "notification_code"
-    t.string   "attachment"
-    t.boolean  "global",               default: false
-    t.datetime "expires"
-  end
-
-  add_index "mailboxer_notifications", ["conversation_id"], name: "index_mailboxer_notifications_on_conversation_id"
-
-  create_table "mailboxer_receipts", force: true do |t|
-    t.integer  "receiver_id"
-    t.string   "receiver_type"
-    t.integer  "notification_id",                            null: false
-    t.boolean  "is_read",                    default: false
-    t.boolean  "trashed",                    default: false
-    t.boolean  "deleted",                    default: false
-    t.string   "mailbox_type",    limit: 25
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
-  end
-
-  add_index "mailboxer_receipts", ["notification_id"], name: "index_mailboxer_receipts_on_notification_id"
-
-  create_table "parameters", force: true do |t|
-    t.string   "name"
-    t.string   "data_type"
-    t.integer  "rule_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "description"
-  end
-
-  create_table "payments", force: true do |t|
-    t.integer  "company_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "plan_types", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "plans", force: true do |t|
-    t.integer  "plan_type_id"
-    t.string   "interval"
-    t.string   "currency"
-    t.float    "price"
-    t.string   "paymill_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "regions", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "company_id"
-    t.datetime "deleted_at"
-  end
-
-  add_index "regions", ["deleted_at"], name: "index_regions_on_deleted_at"
-
-  create_table "roles", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "rule_notifications", force: true do |t|
-    t.integer  "rule_id"
-    t.integer  "car_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "rules", force: true do |t|
-    t.string   "name"
-    t.string   "method_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "description"
-  end
-
-  create_table "simcards", force: true do |t|
-    t.string   "telephone_number"
-    t.integer  "teleprovider_id"
-    t.float    "monthly_price"
-    t.integer  "device_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "company_id"
-    t.datetime "deleted_at"
-    t.string   "name"
-  end
-
-  add_index "simcards", ["deleted_at"], name: "index_simcards_on_deleted_at"
-
-  create_table "states", force: true do |t|
-    t.boolean  "no_data",    default: false
-    t.boolean  "moving",     default: false
-    t.integer  "car_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.float    "speed",      default: 0.0
-    t.integer  "driver_id"
-    t.integer  "device_id"
-  end
-
-  create_table "subscriptions", force: true do |t|
-    t.string   "email"
-    t.string   "name"
-    t.string   "paymill_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "plan_id"
-    t.integer  "company_id"
-    t.boolean  "active"
-  end
-
-  create_table "teleproviders", force: true do |t|
-    t.string   "name"
-    t.string   "apn"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "user_settings", force: true do |t|
+    t.string "speedUnit"
   end
 
   create_table "users", force: true do |t|
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "encrypted_password",     default: ""
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.integer  "company_id"
-    t.string   "invitation_token"
-    t.datetime "invitation_created_at"
-    t.datetime "invitation_sent_at"
-    t.datetime "invitation_accepted_at"
-    t.integer  "invitation_limit"
-    t.integer  "invited_by_id"
-    t.string   "invited_by_type"
-    t.integer  "invitations_count",      default: 0
-    t.integer  "roles_mask"
-    t.integer  "car_id"
-    t.string   "first_name"
-    t.string   "last_name"
+    t.boolean "admin"
+    t.string  "login"
+    t.string  "password"
+    t.integer "userSettings_id", limit: 8
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["invitation_token"], name: "index_users_on_invitation_token", unique: true
-  add_index "users", ["invitations_count"], name: "index_users_on_invitations_count"
-  add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id"
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["userSettings_id"], name: "FK6A68E0862018CAA", using: :btree
 
-  create_table "vertices", force: true do |t|
-    t.float    "latitude"
-    t.float    "longitude"
-    t.integer  "region_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "company_id"
+  create_table "users_devices", id: false, force: true do |t|
+    t.integer "users_id",   limit: 8, null: false
+    t.integer "devices_id", limit: 8, null: false
   end
 
-  create_table "work_hours", force: true do |t|
-    t.integer  "day_of_week"
-    t.datetime "starts_at"
-    t.datetime "ends_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "work_schedule_id"
-    t.integer  "company_id"
-    t.datetime "deleted_at"
-  end
+  add_index "users_devices", ["devices_id"], name: "FK81E459A68294BA3", using: :btree
+  add_index "users_devices", ["users_id"], name: "FK81E459A6712480D", using: :btree
 
-  add_index "work_hours", ["deleted_at"], name: "index_work_hours_on_deleted_at"
-  add_index "work_hours", ["work_schedule_id"], name: "index_work_hours_on_work_schedule_id"
+  add_foreign_key "devices", "positions", name: "FK5CF8ACDD7C6208C3", column: "latestPosition_id"
 
-  create_table "work_schedule_groups", force: true do |t|
-    t.integer  "company_id"
-    t.integer  "work_schedule_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name"
-  end
+  add_foreign_key "positions", "devices", name: "FK65C08C6ADB0C3B8A"
 
-  create_table "work_schedules", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "company_id"
-    t.datetime "deleted_at"
-  end
+  add_foreign_key "users", "user_settings", name: "FK6A68E0862018CAA", column: "userSettings_id"
 
-  add_index "work_schedules", ["deleted_at"], name: "index_work_schedules_on_deleted_at"
+  add_foreign_key "users_devices", "devices", name: "FK81E459A68294BA3", column: "devices_id"
+  add_foreign_key "users_devices", "users", name: "FK81E459A6712480D", column: "users_id"
 
 end
