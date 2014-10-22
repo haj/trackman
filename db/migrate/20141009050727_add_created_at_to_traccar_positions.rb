0511080ab126fdc979c5880348991bc49b9b0508
Zakaria Braksa <zakaria.braksa@gmail.com>
2014-10-22 20:33:08 +0100

    Traccar migration fix

--- db/migrate/20141009050727_add_created_at_to_traccar_positions.rb
+++ db/migrate/20141009050727_add_created_at_to_traccar_positions.rb
@@ -1,11 +1,9 @@
 class AddCreatedAtToTraccarPositions < ActiveRecord::Migration
   
-	ActiveRecord::Base.establish_connection("secondary_#{Rails.env}".to_sym)
-
   def change
-  	ActiveRecord::Base.connection.initialize_schema_migrations_table
+  	ActiveRecord::Base.establish_connection("secondary_#{Rails.env}".to_sym)
     execute "ALTER TABLE `positions` ADD `created_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;" 
-    #@connection = ActiveRecord::Base.establish_connection("#{Rails.env}").connection
+    ActiveRecord::Base.establish_connection "#{Rails.env}"
   end
 end
 
