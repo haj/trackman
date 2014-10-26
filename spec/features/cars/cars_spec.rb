bf87478a1b20216fa2e2ff6f0918bfffb5701850
Zakaria Braksa <zakaria.braksa@gmail.com>
2014-10-26 12:42:32 +0000

    Wrote rspec tests for creating/editing cars

--- spec/features/cars/cars_spec.rb
+++ spec/features/cars/cars_spec.rb
@@ -5,57 +5,54 @@ Warden.test_mode!
 describe "device management" do
 
     before (:each) do
-      CarModel.create(name: "Audi A2")
-      CarType.create(name: "Sports Car")
-      # sign in manager
-      user = FactoryGirl.create(:manager) 
+      # Sign in manager
+      user = FactoryGirl.create(:admin) 
       login_as user, scope: :user
       ActsAsTenant.current_tenant = Company.first
       car = FactoryGirl.create(:car)
     end
 
-    after(:each) do
+    after(:all) do
       ActsAsTenant.current_tenant = nil 
     end
 
   it "should allow to create new car" do 
     visit new_car_path
-    #page.status_code.should be 200
     page.should have_content("Sign out")
-    # fill_in "car_numberplate", :with => "211849"
-    # select  "Audi A2", :from => "car_car_model_id"
-    # select  "Sports Car", :from => "car_car_type_id"
-    # fill_in "car_registration_no", :with => "1"
-    # fill_in "car_year", :with => "1"
-    # click_button "Save"
+    fill_in "car_name", :with => "NewCar"
+    fill_in "car_numberplate", :with => "211849"
+    fill_in "car_mileage", :with => "1"
+    fill_in "car_registration_no", :with => "1"
+    fill_in "car_color", :with => "Red"
+    fill_in "car_year", :with => "1"
+    click_button "Save"
 
-    # Car.where(numberplate: "211849").should exist
-    # page.should have_content("Car was successfully created")
+    #Car.where(numberplate: "211849").count.should_not eq(0)
+    page.should have_content("was successfully created")
   end
+
+
+
 
   it "should allow to destroy a car" do 
     visit cars_path 
-    page.status_code.should be 200
-    # expect { click_link 'Destroy' }.to change(Car, :count).by(-1)
+    #expect { click_link 'Destroy' }.to change(Car, :count).by(-1)
   end
 
   it "should allow to list all cars" do 
     visit cars_path
-    page.status_code.should be 200
-    # page.should have_content('NewCar')
+    page.should have_content('Vehicles')
   end
 
   it "should allow to edit a car model" do 
     visit edit_car_path(Car.first)
-    page.status_code.should be 200
-    # page.should have_css('#car_numberplate')
 
-    # fill_in "car_numberplate", :with => "NewCar2"
-    # click_button "Save"
+    fill_in "car_numberplate", :with => "NewCar2"
+    click_button "Save"
 
-    # Car.where(numberplate: "NewCar").should_not exist
-    # Car.where(numberplate: "NewCar2").should exist
-    # page.should have_content('Sign out')
+    Car.where(numberplate: "NewCar").should_not exist
+    Car.where(numberplate: "NewCar2").should exist
+    page.should have_content('Sign out')
   end
 
 
