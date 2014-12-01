43c267ec83be4504c960708d99ddf4d88cc5292b
Zakaria Braksa <zakaria.braksa@gmail.com>
2014-12-01 00:10:15 +0000

    Fix : when user select custom start/end date, also show feedback when there is no positions during a specific start/end dates

--- app/models/car.rb
+++ app/models/car.rb
@@ -203,9 +203,9 @@ class Car < ActiveRecord::Base
 		if dates.nil?
 			self.device.traccar_device.positions.order("time DESC").take(30)
 		else
-			start_date = Time.zone.parse("#{user_dates[:start_date]} #{user_dates[:start_time]}")
-			end_date = Time.zone.parse("#{user_dates[:end_date]} #{user_dates[:end_time]}")
-			user_dates[:limit_results] = 20 if user_dates[:limit_results].to_i == 0 
+			start_date = Time.zone.parse("#{dates[:start_date]} #{dates[:start_time]}")
+			end_date = Time.zone.parse("#{dates[:end_date]} #{dates[:end_time]}")
+			dates[:limit_results] = 20 if dates[:limit_results].to_i == 0 
 			return self.device.traccar_device.positions.where("created_at > ? AND created_at < ?", start_date.to_s(:db), end_date.to_s(:db)).order("time DESC")#.take(user_dates[:limit_results].to_i)
 		end
 	end
