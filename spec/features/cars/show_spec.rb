82ce529c2b9a9ee931d35176f5e4e9e85ee78588
Zakaria Braksa <zakaria.braksa@gmail.com>
2014-12-28 20:25:20 +0000

    Implemented more rspec tests to make sure cars#show shows the exact positions according to time slot selected by user

--- spec/features/cars/show_spec.rb
+++ spec/features/cars/show_spec.rb
@@ -8,7 +8,7 @@ describe Car do
     include_context "sign_in"
     include_context "sign_out"
 
-    before (:each) do
+    before (:all) do #revert to each if it doesn't work
       Time.zone = "GMT" 
       Traccar::Device.destroy_all
       @car = FactoryGirl.create(:car)
@@ -22,6 +22,17 @@ describe Car do
         # move time by 15 minutes
         first_day += 900 #900 seconds = 15 minutes
       end 
+    end
+
+    it "Should show the exact positions between two dates (and previous or later dates)" do
+      visit car_path(@car) 
+      fill_in "dates_start_date", with: "4/12/2014"
+      fill_in "dates_start_time", with: "08:15"
+      fill_in "dates_end_date", with: "4/12/2014"
+      fill_in "dates_end_time", with: "09:00"
+      click_button "Apply" 
+      expect(page).to have_content("8:15")
+      expect(page).to_not have_content("8:00")
     end
 
     it "Should show some positions by default" do
