61d3c0ae2fc72c1f2b1e6ea06e6951b8ab522f92
Zakaria Boualaid <randomacts@MacBook-de-Zakaria.local>
2016-01-17 15:39:46 +0000

    Bad fix for CSRF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  skip_before_action :verify_authenticity_token
+
   helper_method :current_tenant
   helper_method :js_class_name
 
