63f3cf8bfeeb7112fcd9ba05e7b4264c65620a0c
Zakaria Boualaid <boulaidzac@gmail.com>
2016-05-17 18:51:18 +0100

    Fix speed Knot => Km/h

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -10,7 +10,7 @@ class LocationsController < ApplicationController
     position_id = params[:position_id]
     fix_time = params[:fix_time]
     valid = params[:valid]
-    speed = params[:speed]
+    speed = params[:speed] # Speed in Knots
     status = params[:status]
     device = Device.find_by_emei(unique_id)
 
@@ -18,14 +18,11 @@ class LocationsController < ApplicationController
     jsoned_xml = JSON.pretty_generate(Hash.from_xml(position.other))
     ignite = JSON[jsoned_xml]["info"]["power"]
 
-    puts "time ==>"
-    puts fix_time
+    # Conversion of speed from knots to km/h
+    speed = speed.to_f * 1.852
 
     l = Location.create(device_id: device.id, latitude: lat, longitude: lon, time: fix_time, speed: speed, valid_position: valid,
         position_id: position_id, status: status)
-
-    puts l.time.utc
-    puts l.time.to_date
 
     if ignite != ""
         l.ignite = ignite
