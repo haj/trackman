0012b684dd43fb16c99d268f30960d6ee5d5aa21
Zakaria Boualaid <boulaidzac@gmail.com>
2016-06-05 10:35:31 +0000

    Sidekiq config for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,8 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
+set :sidekiq_processes => 2
+set :sidekiq_config, "#{current_path}/config/sidekiq.yml"
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
