c060636de69381849b4282d1d91bcc14ad5f8842
Andreas Arnol <andreasarnold02@gmail.com>
2016-08-16 22:04:00 +0700

    Scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 # set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
-set :environment, "production"
+set :environment, "development"
 
 every 1.minute do 
   rake "alarms:check"
