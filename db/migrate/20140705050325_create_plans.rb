d25e8960d562ea64d2172fac0a9c135c43d65d8f
Andreas Arnol <andreasarnold02@gmail.com>
2016-08-29 22:22:08 +0700

    Db

--- db/migrate/20140705050325_create_plans.rb
+++ db/migrate/20140705050325_create_plans.rb
@@ -1,13 +1,15 @@
 class CreatePlans < ActiveRecord::Migration
   def change
-    create_table :plans do |t|
-      t.integer :plan_type_id
-      t.string :interval
-      t.string :current
-      t.float :price
-      t.string :paymill_id
+    unless Plan.table_exists?
+      create_table :plans do |t|
+        t.integer :plan_type_id
+        t.string :interval
+        t.string :current
+        t.float :price
+        t.string :paymill_id
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
