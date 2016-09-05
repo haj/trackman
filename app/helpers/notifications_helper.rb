ae484a9aafdccdd1af75b15b98989efd82acc69a
Andreas Arnol <andreasarnold02@gmail.com>
2016-09-05 23:48:40 +0700

    Decline

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -5,13 +5,19 @@ module NotificationsHelper
 		end		
 	end
 
-	def accept_decine_helper(action)
+	def accept_decine_helper(action, id)
 		if action == 'assigned'
 			a = ""
       a += "&nbsp; &nbsp;" 
       a += link_to 'Accept', "", class: 'btn btn-mini btn-primary'
       a += "&nbsp;" 
-      a += link_to 'Decline', "", class: 'btn btn-mini btn-danger'
+      a += link_to 'Decline', decline_order_path(id), class: 'btn btn-mini btn-danger',
+      	method: 'put', data: { 
+      		id: id,
+      		confirm: 'Are you sure want to decline this package?', 
+      		type: 'warning',
+      		text: 'Provide a reason.'
+      	}
 		end				
 	end
 end
